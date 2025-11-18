87aa00e40c4ad0ac4f02a65117590b183a5dffe4
Vera Ek Lundqvist <veraeklundqvist@gmail.com>
2025-11-18 11:15:17 +0100

    list all products function

---
+++ src/list_all_products.py
@@ -0,0 +1,19 @@
+from reader import load_json
+
+def list_all_products():
+    data = load_json('dataset/products.json')
+    for id, info in data.items():
+        article_id = id
+        product = info['article_name']
+        brand = info['brand']
+        price = info['price_SEK']
+        discount = info['discount_percentage']
+
+        print(f"""art#{article_id}
+-------
+Name: {product}
+Brand: {brand}
+Standard price: {price}
+Current discount: {discount}
+Current price: {price - int(discount * 100)}""")
+        
